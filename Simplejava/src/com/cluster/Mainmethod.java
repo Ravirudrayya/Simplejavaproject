8fbf0f40ba99cbe38642bf82e2ebc089aeeea59e
abc <abc@DESKTOP-HOM67QC>
2019-07-05 22:24:00 +0530

    "This is Third commit"

--- Simplejava/src/com/cluster/Mainmethod.java
+++ Simplejava/src/com/cluster/Mainmethod.java
@@ -1,7 +1,12 @@
 package com.cluster;
 
-public class Mainmethod {
+public class Mainmethod 
+{
 
+	public  static void m1()
+	{
+		System.out.println("user defined method");
+	}
 	public static void main(String[] args) 
 	{
 		int a=99;
