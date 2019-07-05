423c54e06ab4180c6365ba0d24729c700f47ed9b
abc <abc@DESKTOP-HOM67QC>
2019-07-05 22:28:05 +0530

    "This is fourth commit"

--- Simplejava/src/com/cluster/Mainmethod.java
+++ Simplejava/src/com/cluster/Mainmethod.java
@@ -3,9 +3,11 @@ package com.cluster;
 public class Mainmethod 
 {
 
+	
+	static  int  a=10;
 	public  static void m1()
 	{
-		System.out.println("user defined method");
+		System.out.println("user defined method"+a);
 	}
 	public static void main(String[] args) 
 	{
