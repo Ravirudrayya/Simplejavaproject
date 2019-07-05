eba4d796e56871f00a99f57154643b6100a69cd7
abc <abc@DESKTOP-HOM67QC>
2019-07-05 21:14:39 +0530

    this is second commit

--- Simplejava/src/com/cluster/Mainmethod.java
+++ Simplejava/src/com/cluster/Mainmethod.java
@@ -2,7 +2,9 @@ package com.cluster;
 
 public class Mainmethod {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		int a=99;
 		System.out.println("hello github");
 
 	}
